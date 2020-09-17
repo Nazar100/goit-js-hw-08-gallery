5ffe803d55bd61913648dedf436c9e9c22f7021f
Nazar100 <leshc005@umn.edu>
2020-09-17 16:34:52 +0300

    proccess

--- gallery-items.js
+++ gallery-items.js
@@ -1,10 +1,24 @@
 export default [
   {
     preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
       "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
     original:
       "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
     description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg",
+    description: "Aerial Beach View",
   },
   {
     preview:
--- gallery.js
+++ gallery.js
@@ -1,47 +1,67 @@
-import images from "./gallery-items.js";
+import images from './gallery-items.js';
 
-const galleryRef = document.querySelector(".js-gallery");
-const largeImageRef = document.querySelector(".lightbox__image");
-const modalContainerRef = document.querySelector(".js-lightbox");
+const galleryRef = document.querySelector('.js-gallery');
+const largeImageRef = document.querySelector('.lightbox__image');
+const modalContainerRef = document.querySelector('.js-lightbox');
 const closeButtonRef = document.querySelector(
-  'button[data-action="close-lightbox"]'
+    'button[data-action="close-lightbox"]',
 );
-const overlayRef = document.querySelector(".js-overlay");
+const overlayRef = document.querySelector('.js-overlay');
+const nextRef = document.querySelector('.arrow-next');
 
-const imagesList = images.map((image) => {
-  const list = document.createElement("li");
-  const photo = document.createElement("img");
-  list.append(photo);
-  photo.src = image.preview;
-  photo.alt = image.description;
-  photo.setAttribute("original", image.original);
-  photo.classList.add("gallery__image", "gallery__link");
-  list.classList.add("gallery__item");
-  return list;
+const imagesList = images.map((image, index) => {
+    const list = document.createElement('li');
+    const link = document.createElement('a');
+    const photo = document.createElement('img');
+    link.appendChild(photo);
+    list.appendChild(link);
+    photo.src = image.preview;
+    photo.alt = image.description;
+    photo.dataset.source = image.original;
+    photo.dataset.index = index;
+    photo.classList.add('gallery__image');
+    link.classList.add('gallery__link');
+    list.classList.add('gallery__item');
+    return list;
 });
 galleryRef.append(...imagesList);
 
-galleryRef.addEventListener("click", galleryClick);
-closeButtonRef.addEventListener("click", closeModal);
-overlayRef.addEventListener("click", closeModal);
-
+galleryRef.addEventListener('click', galleryClick);
+closeButtonRef.addEventListener('click', closeModal);
+overlayRef.addEventListener('click', closeModal);
+nextRef.addEventListener('click', nextImage);
+let array = [];
 function galleryClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  largeImageRef.src = event.target.getAttribute("original");
-  largeImageRef.alt = event.target.alt;
-  openModal();
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    largeImageRef.src = event.target.dataset.source;
+    largeImageRef.alt = event.target.alt;
+    largeImageRef.dataset.index = event.target.dataset.index;
+
+    openModal();
 }
+
+images.map((image, index) => {
+    image.dataset.index = index;
+});
+
+console.log(images);
 
 function openModal() {
-  modalContainerRef.classList.add("is-open");
+    modalContainerRef.classList.add('is-open');
 }
 
-function closeModal() {
-  if (event.target.nodeName !== "IMG") {
-    largeImageRef.src = "";
-    modalContainerRef.classList.remove("is-open");
-  }
+function closeModal(event) {
+    if (event.target.nodeName !== 'IMG' && event.target !== nextRef) {
+        largeImageRef.src = '';
+        modalContainerRef.classList.remove('is-open');
+    }
 }
+function nextImage() {
+    largeImageRef.src = [largeImageRef.src++];
+    console.log([largeImageRef.dataset.index++]);
+    console.log(largeImageRef.dataset.index, largeImageRef);
+}
+console.log(array);
